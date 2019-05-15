6250ed5577a4c53cba11ab8be2ea148d2a3fba33
ck222re <csk87@live.se>
2019-05-15 19:27:04 +0200

    Added Skeleton-code

---
+++ app.js
@@ -0,0 +1,8 @@
+const Koa = require('koa')
+const app = new Koa()
+
+const port = 3000
+
+app.use(async ctx => (ctx.body = 'Hello World'))
+
+app.listen(port, () => console.log('server is running on port: ' + port))
